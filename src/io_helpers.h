9e20f9edae024e14ce308b2f2afcc3d6d4dac151
adrien1018 <adrien1018@users.noreply.github.com>
2023-11-10 08:52:26 +0800

    Finish position edge

---
+++ src/io_helpers.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <cstdint>
+#include <vector>
+#include "constexpr_helpers.h"
+
+template <size_t size_bytes, class T, class Func>
+inline size_t VecOutput(const std::vector<T>& vec, uint8_t data[], Func&& func) {
+  uint8_t sz_buf[8] = {};
+  IntToBytes<uint64_t>(vec.size(), sz_buf);
+  memcpy(data, sz_buf, size_bytes);
+  size_t ind = size_bytes;
+  for (auto& i : vec) ind += func(i, data + ind);
+  return ind;
+}
+
+template <size_t size_bytes, class T, class Func>
+inline size_t VecInput(std::vector<T>& vec, const uint8_t data[], Func&& func) {
+  uint8_t sz_buf[8] = {};
+  memcpy(sz_buf, data, size_bytes);
+  vec.resize(BytesToInt<uint64_t>(sz_buf));
+  size_t ind = size_bytes;
+  for (auto& i : vec) ind += func(i, data + ind);
+  return ind;
+}
+
+template <size_t size_bytes, class T>
+inline size_t SimpleVecOutput(const std::vector<T>& vec, uint8_t data[]) {
+  uint8_t sz_buf[8] = {};
+  IntToBytes<uint64_t>(vec.size(), sz_buf);
+  memcpy(data, sz_buf, size_bytes);
+  memcpy(data + size_bytes, vec.data(), sizeof(T) * vec.size());
+  return sizeof(T) * vec.size() + size_bytes;
+}
+
+template <size_t size_bytes, class T>
+inline size_t SimpleVecInput(std::vector<T>& vec, const uint8_t data[]) {
+  uint8_t sz_buf[8] = {};
+  memcpy(sz_buf, data, size_bytes);
+  vec.resize(BytesToInt<uint64_t>(sz_buf));
+  memcpy(vec.data(), data + size_bytes, sizeof(T) * vec.size());
+  return sizeof(T) * vec.size() + size_bytes;
+}
